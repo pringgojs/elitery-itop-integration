554da16bd27bf64760829fefbd51b64f4183f5b8
Pringgo Juni S <odyinggo@gmail.com>
2026-03-31 15:40:22 +0700

    refactor logging and mapping checks in ticket update methods

--- app/Http/Controllers/Api/V1/ItopEliteryReciverController.php
+++ app/Http/Controllers/Api/V1/ItopEliteryReciverController.php
@@ -6,6 +6,7 @@ use App\Constants\Constants;
 use App\Http\Controllers\Controller;
 use App\Jobs\ProcessTicketStateChangeJob;
 use App\Jobs\ProcessTicketUpdateLogFromElitery;
+use App\Models\TicketMapping;
 use Illuminate\Http\Request;
 
 class ItopEliteryReciverController extends Controller
@@ -13,7 +14,15 @@ class ItopEliteryReciverController extends Controller
     public function ticketUpdatePrivateLog(Request $request)
     {
         $data = $request->all();
-        info('Received JSON payload for update private log', $data);
+        info('ELITERY - update private log', $data);
+        $mapping = TicketMapping::where('elitery_ticket_id', $data['id'])->first();
+        if (! $mapping) {
+            info("No mapping found for ticket id: " . $data['id']);
+            return response()->json([
+                'message' => 'No mapping found for ticket id: ' . $data['id']
+            ], 404);
+        }
+        
         ProcessTicketUpdateLogFromElitery::dispatch($data['id']);
         return response()->json([
             'received' => $data
@@ -25,7 +34,15 @@ class ItopEliteryReciverController extends Controller
     public function ticketStateChange(Request $request)
     {
         $data = $request->all();
-        info('update state', $data);
+        info('ELITERY - update state', $data);
+        $mapping = TicketMapping::where('elitery_ticket_id', $data['id'])->first();
+        if (! $mapping) {
+            info("No mapping found for ticket id: " . $data['id']);
+            return response()->json([
+                'message' => 'No mapping found for ticket id: ' . $data['id']
+            ], 404);
+        }
+
         ProcessTicketStateChangeJob::dispatch($data['id']);
         return response()->json([
             'received' => $data
